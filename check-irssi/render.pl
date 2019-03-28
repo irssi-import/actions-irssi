7fe668a3ac881de47e4f8018e09bbba26ca6251d
Ailin Nemui <ailin@z30a.localdomain>
2019-03-28 13:32:20 +0100

    basic scrolling

--- check-irssi/render.pl
+++ check-irssi/render.pl
@@ -10,6 +10,7 @@ my $LINES   = $ENV{LINES}   || 24;
 
 my $CL = 0;
 my $CC = 0;
+my @sr = (-1, -1);
 
 my $input = do { local $/; <> };
 
@@ -17,11 +18,24 @@ while ($input =~ /(?<ctl>\e (
 			(?<keypad> [=>] )
 		      | \( (?<charset> ( % . | . ))
 		      | (?<priv> \[\? (?<pcontent> [0-9:;]*?) (?<pcsi>) [hl] )
-		      | \[ (?<ccontent> [0-9:;]*?) (?<csi> [mtHKb] )))
+		      | \[ (?<ccontent> [0-9:;]*?) (?<csi> [mtHKbrS] )))
 	      | (?<sp> [\r\n] )
 	      | (?<oth> . )/sxg) {
     if (length $+{csi}) {
 	my $csi = $+{csi};
+	if ($csi eq 'S') {
+	    my $ccontent = $+{ccontent};
+	    $ccontent = 1 unless length $ccontent;
+
+	    my $from = max($sr[ 0 ], 0);
+	    my $removed = min( $ccontent, ($sr[ 1 ] == -1 ? scalar @grid : $sr[ 1 ]) - ($sr[ 0 ] == -1 ? 0 : $sr[ 0 ]) );
+
+	    if ($sr[ 1 ] != -1 && @grid > $sr[ 1 ]) {
+		splice @grid, $sr[ 1 ], 0, ([]) x $removed;
+	    }
+	    splice @grid, $from, $removed;
+	    next;
+	}
 	if ($csi eq 'b') {
 	    my $ccontent = $+{ccontent};
 	    if (length $ccontent) {
@@ -31,25 +45,32 @@ while ($input =~ /(?<ctl>\e (
 		next;
 	    }
 	}
-	if ($csi eq 'K') {
+	if ($csi eq 'r') {
 	    my $ccontent = $+{ccontent};
 	    if (length $ccontent) {
-		if ($ccontent == 0) {
-		    splice @{$grid[ $CL ]}, $CC;
-		    next;
-		}
-		if ($ccontent == 1) {
-		    $grid[ $CL ][$_] = undef
-			for 0 .. min($#{ $grid[ $CL ] }, $CC);
-		    next;
-		}
-		if ($ccontent == 2) {
-		    @{ $grid[ $CL ] } = ();
-		    next;
-		}
+		my ($top, $bot) = split ';', $ccontent;
+		@sr = ($top - 1, $bot - 1);
+		next;
 	    }
 	    else {
+		@sr = (-1, -1);
+		next;
+	    }
+	}
+	if ($csi eq 'K') {
+	    my $ccontent = $+{ccontent};
+	    $ccontent = 0 unless length $ccontent;
+	    if ($ccontent == 0) {
 		splice @{$grid[ $CL ]}, $CC;
+		next;
+	    }
+	    if ($ccontent == 1) {
+		$grid[ $CL ][$_] = undef
+		    for 0 .. min($#{ $grid[ $CL ] }, $CC);
+		next;
+	    }
+	    if ($ccontent == 2) {
+		@{ $grid[ $CL ] } = ();
 		next;
 	    }
 	}
@@ -67,6 +88,11 @@ while ($input =~ /(?<ctl>\e (
 		next;
 	    }
 	}
+	if ($csi eq 't' || $csi eq 'm') {
+	    # ignore
+	    next;
+	}
+	die "unhandled csi: $csi";
     }
     if (length $+{sp}) {
 	my $sp = $+{sp};
@@ -78,6 +104,7 @@ while ($input =~ /(?<ctl>\e (
 	    $CC++;
 	    next;
 	}
+	die "unhandled sp: $sp";
     }
     if (length $+{oth}) {
 	my $oth = $+{oth};
@@ -92,15 +119,20 @@ while ($input =~ /(?<ctl>\e (
     }
 }
 
-print '╒' . ( '═' x $COLUMNS ) . '╕' . "\n";
-for my $line (@grid) {
-    print '│';
-    my $i = 0;
-    for my $col (@$line) {
-	print $col // ' ';
-	$i++;
+sub _dump {
+    my @grid = @_;
+    print '╒' . ( '═' x $COLUMNS ) . '╕' . "\n";
+    for my $line (@grid) {
+	print '│';
+	my $i = 0;
+	for my $col (@$line) {
+	    print $col // ' ';
+	    $i++;
+	}
+	print ' ' x max($COLUMNS - $i, 0) . '│';
+	print "\n";
     }
-    print ' ' x max($COLUMNS - $i, 0) . '│';
-    print "\n";
+    print '╘' . ( '═' x $COLUMNS ) . '╛' . "\n";
 }
-print '╘' . ( '═' x $COLUMNS ) . '╛' . "\n";
+
+_dump(@grid);
